29ac47bf0e95ee405b57095d58087987594a970b
baituole,yeshifu <1143289550@qq.com>
2023-12-15 23:08:47 +0800

    add：字节数组转换为8bit灰度位图

---
+++ OST.Utils/Images/ImageUtil.cs
@@ -0,0 +1,58 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace OST.Utils.Images
+{
+    public class ImageUtil
+    {
+
+        /// <summary>  
+        /// 字节数组转换为8bit灰度位图  
+        /// </summary>  
+        /// <param name="rawValues">显示字节数组</param>  
+        /// <param name="width">图像宽度</param>  
+        /// <param name="height">图像高度</param>  
+        /// <returns>位图</returns>  
+        public static Bitmap ToGrayBitmap(byte[] rawValues, int width, int height)
+        {
+            //// 申请目标位图的变量，并将其内存区域锁定  
+            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, width, height),
+            ImageLockMode.WriteOnly, PixelFormat.Format8bppIndexed);
+
+            int stride = bmpData.Stride;  // 扫描线的宽度  
+            int offset = stride - width;  // 显示宽度与扫描线宽度的间隙  
+            IntPtr iptr = bmpData.Scan0;  // 获取bmpData的内存起始位置  
+            int scanBytes = stride * height;// 用stride宽度，表示这是内存区域的大小  
+
+            int posScan = 0, posReal = 0;// 分别设置两个位置指针，指向源数组和目标数组  
+            byte[] pixelValues = new byte[scanBytes];  //为目标数组分配内存  
+
+            for (int x = 0; x < height; x++)
+            {
+                for (int y = 0; y < width; y++)
+                {
+                    pixelValues[posScan++] = rawValues[posReal++];
+                }
+                posScan += offset;   
+            }
+
+            System.Runtime.InteropServices.Marshal.Copy(pixelValues, 0, iptr, scanBytes);
+            bmp.UnlockBits(bmpData);  // 解锁内存区域  
+
+            ColorPalette tempPalette;
+            using (Bitmap tempBmp = new Bitmap(1, 1, PixelFormat.Format8bppIndexed))
+            {
+                tempPalette = tempBmp.Palette;
+            }
+            for (int i = 0; i < 256; i++)
+            {
+                tempPalette.Entries[i] = Color.FromArgb(i, i, i);
+            }
+
+            bmp.Palette = tempPalette;
+
+            return bmp;
+        }
+    }
+}
