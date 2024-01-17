a29358d79f8add574d0a0c2a637506e655d7177d
baituole,yeshifu <1143289550@qq.com>
2024-01-18 00:37:45 +0800

    add：ushort数组转成有符号整数

---
+++ OST.Utils/Numbers/ConvertUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OST.Utils.Numbers
+{
+    public class ConvertUtil
+    {
+        public static int UshortArrayToInt(ushort[] hexArray)
+        {
+            //ushort[] hexArray = { 0x0000, 0x64 }; // 替换为你的 ushort 数组
+
+            // 将 ushort 数组转换为有符号整数
+            int result = (short)((hexArray[0] << 16) | hexArray[1]);
+            return result;
+        }
+
+
+    }
+}
